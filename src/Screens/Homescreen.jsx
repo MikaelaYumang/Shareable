dc6317a4d9a9082bc4ca6e17d20e1a28b82993a5
Mikaela Yumang <myyumang@student.hau.edu.ph>
2024-01-18 09:20:31 +0800

    new search test bar

--- src/Screens/Homescreen.jsx
+++ src/Screens/Homescreen.jsx
@@ -12,14 +12,14 @@ function Homescreen() {
 
   useEffect(() => {
     const filtered = data.filter((item) =>
-      item.title.toLowerCase().includes(searchTerm.toLowerCase())
+      item.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
     setFilteredData(filtered);
   }, [data, searchTerm]);
 
   return (
     <>
-      <div className="templateContainer">
+      {/* <div className="templateContainer">
         <div className="searchInput_Container">
           <input
             id="searchInput"
@@ -31,13 +31,32 @@ function Homescreen() {
         <div className="template_Container">
           {filteredData.map((item) => (
             <div className="template" key={item.id}>
-              <img src={item.image} alt={item.title} />
-              <h3>{item.title}</h3>
-              <p className="price">Php{item.price}</p>
+              <img src={item.image} alt={item.name} />
+              <h3>{item.name}</h3>
+              <p>{item.bio}</p>
             </div>
           ))}
         </div>
-      </div>
+      </div> */}
+      <h3 className='text-center my-3'>Search for Sharers you love!</h3>
+      <div className="searchBox">
+            <input
+              id="searchInput"
+              className="searchInput"
+              type="text"
+              placeholder="Search here..."
+              onChange={(event) => setSearchTerm(event.target.value)}
+            />
+            <div className="templateContainer">
+              {filteredData.map((item) => (
+                <div className="template" key={item.id}>
+                  <img src={item.image} alt={item.name} />
+                  <h3>{item.name}</h3>
+                  <p>{item.bio}</p>
+                </div>
+              ))}
+            </div>
+          </div>
     </>
   );
 }
